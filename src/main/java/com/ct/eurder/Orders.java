cac93988fbb69aa3047ebd1606065ccc5334b0ee
Carl Theuns <carl.theuns@gmail.com>
2023-04-01 18:02:41 +0200

    story 3 additemGroup stuck

--- src/main/java/com/ct/eurder/Orders.java
+++ src/main/java/com/ct/eurder/Orders.java
@@ -1,12 +1,20 @@
 package com.ct.eurder;
 
+import com.ct.eurder.items.Itemgroup;
+import com.ct.eurder.items.ItemsRepositery;
+
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 public class Orders {
 
+    private  Map<UUID, Itemgroup> order;
+
+    public Orders(Map<UUID, Itemgroup> order) {
+        this.order = new HashMap<>();
+    }
 
 
-    private Map<UUID,ItemGroup>>;
 }
--- src/main/java/com/ct/eurder/items/ItemsRepositery.java
+++ src/main/java/com/ct/eurder/items/ItemsRepositery.java
@@ -1,27 +1,51 @@
 package com.ct.eurder.items;
 
+import com.ct.eurder.Orders;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Stream;
 
 @Repository
 public class ItemsRepositery {
 
-    Map<Integer, Items> itemsDatabese;
-    int key = 0;
+    Map<UUID, Items> itemsDatabese;
 
     public ItemsRepositery() {
         this.itemsDatabese = new HashMap<>();
     }
 
     public void addItem(Items itemInput) {
-        key++;
+        UUID key = UUID.randomUUID();
         itemsDatabese.put(key, itemInput);
     }
 
     public List<Items> getAllItems() {
         return List.copyOf(itemsDatabese.values());
     }
+
+    public boolean isItemInStock(Items items){
+       return itemsDatabese.containsKey(items.getItemId());
+    }
+
+    public boolean isEnoughInStock(Items items,Itemgroup itemgroup){
+        return (items.getStockAmount() >= itemgroup.getOrderAmount());
+    }
+
+
+    public void addItemGroup(Itemgroup itemgroup, Orders order){
+        UUID orderId = UUID.randomUUID();
+       Items soldItem = itemsDatabese.get(itemgroup.getItemToOrderId());
+        int supplyAmount = soldItem.getStockAmount();
+        int neededAmount = itemgroup.getOrderAmount();
+        if (isItemInStock(soldItem) && supplyAmount >= neededAmount){
+            order.put(orderId,itemgroup);
+
+        }
+    }
+
+
 }
